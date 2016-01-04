460017278c6bd8da396f7461522934bf8ae08590
adelmoral <adelmoral13@gmail.com>
2016-01-04 19:23:21 -0400

    lots of progress on rsvp, backend working but I still need to polish

---
+++ app/controllers/parties_controller.rb
@@ -0,0 +1,62 @@
+class PartiesController < ApplicationController
+  def new
+    @party = Party.new
+  end
+
+  def index
+    @parties = Party.all
+  end
+
+  def show
+    if params.has_key?(:party_name)
+      @party = Party.find_by(party_name: params[:party_name])
+    else
+      @party = Party.find(params[:id])
+    end
+  end
+
+  def create
+    @party = Party.new(party_params)
+    if @party.save
+      flash[:notice] = "Party successfully created"
+      redirect_to @party
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    if params.has_key?(:party_name)
+      @party = Party.find_by(party_name: params[:party_name])
+    else
+      @party = Party.find(params[:id])
+    end
+  end
+
+  def update
+    @party = Party.find(params[:id])
+    if @party.update_attributes(party_params)
+      flash[:notice] = "Your RSVP has been received!"
+      redirect_to '/home'
+    else
+      render 'edit'
+    end
+  end
+
+  def search
+    @party = Party.find(params[:search])
+    redirect_to @party
+  end
+
+  def destroy
+    @party = Party.find(params[:id])
+    @party.destroy
+    redirect_to parties_path
+  end
+
+  private
+    def party_params
+      params.require(:party).permit(:rsvp_code, :party_name, :num_guests, :num_nights, :child, :rsvp_guests, :rsvp_nights, :guest_names, :email)
+    end
+
+end
